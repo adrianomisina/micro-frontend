5c94a684d6bdffb395b69a775ea7b0f76b7b6123
Adriano Misina <adriano.misina@agroboard.com.br>
2023-10-24 17:15:39 -0300

    setup server

---
+++ pbp/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+import SafeComponent from "./SafeComponent";
+import Header from "home/Header";
+import Footer from "home/Footer";
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <SafeComponent>
+      <Header />
+    </SafeComponent>
+    <div className="my-10">
+      PDP Page Content
+    </div>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
+
