a4428b542b88380173676e73c0fac076bdcb6dc0
Adriano Misina <adriano.misina@agroboard.com.br>
2023-10-25 14:19:11 -0300

    sharing functions

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Header from './Header';
 import Footer from './Footer';
+import HomeContent from './HomeContent';
 
 const App = () => (
   <div className='text-3xl mx-auto max-w-6xl'>
-      <Header />
-    <div className='my-10'>Home Page Content</div>
+    <Header />
+    <div className='my-10'>
+      <HomeContent />
+    </div>
     <Footer />
   </div>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
-//32:00
+//41:34
---
+++ pbp/src/PDPContent.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+
+const PDPContent = () => {
+    const product = 1;
+  return (
+    <div>Product {product}</div>
+  )
+}
+
+export default PDPContent
