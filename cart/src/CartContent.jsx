79fd0596fcbe96dd58e82beeaa371abed753e51f
Adriano Misina <adriano.misina@agroboard.com.br>
2023-10-27 20:35:44 -0300

    sharing the jwt

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,11 +1,21 @@
 import React, {useState, useEffect} from 'react';
+
+import Login from './Login'
 import {login, jwt} from './cart'
 
 const CartContent = () => {
     const [token, setToken] = useState('');
 
+    useEffect(() =>{
+      // login("sally", "123");
+      return jwt.subscribe((val) => setToken(val ?? ""))
+    },[])
+
   return (
-    <div>JWT: {token}</div>
+    <div>
+      <div>JWT: {token}</div>
+      <Login />    
+    </div>
   )
 }
 
